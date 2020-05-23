fe8056de8a4b7f4c006017ac6ee216226b64b392
MeliMack <Mack.Melisa@gmail.com>
2020-05-22 21:40:04 -0300

    Ciudades agregado

---
+++ Desktop/JS/app.js
@@ -0,0 +1,7 @@
+function count(desde, hasta) {
+    var desde=0;
+    while (desde<hasta){
+            console.log("El número actual es: " + desde + " y es menor a"+hasta);
+            desde++
+}
+}
