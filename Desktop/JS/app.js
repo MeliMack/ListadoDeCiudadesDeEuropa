593877c47b1fb9629394a51f6cdaabf903933bad
MeliMack <65793631+MeliMack@users.noreply.github.com>
2020-05-22 21:55:03 -0300

    Delete app.js

--- Desktop/JS/app.js
+++
@@ -1,7 +0,0 @@
-function count(desde, hasta) {
-    var desde=0;
-    while (desde<hasta){
-            console.log("El número actual es: " + desde + " y es menor a"+hasta);
-            desde++
-}
-}
